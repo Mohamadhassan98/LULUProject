d1cfada9af714b00a7cbf8ebe4cd75a36dc5e273
amirreza <amirreza.seddighin1376@gmail.com>
2019-01-25 14:23:12 +0700

    hello world

--- src/LULU/LuluListener.java
+++ src/LULU/LuluListener.java
@@ -2,6 +2,7 @@ package LULU;
 
 import LULU.Lulu2Parser.FtDclContext;
 import LULU.Lulu2Parser.FuncDclContext;
+import LULU.Lulu2Parser.TypeDclContext;
 import LULU.Lulu2Parser.VarDefContext;
 import LULU.Tree.DeclareScope;
 import LULU.Tree.Node;
@@ -39,13 +40,13 @@ public class LuluListener extends Lulu2BaseListener
 //                    if (typee == null)
 //                    {
                     var typee = ((DeclareScope) currentNode.getScope()).getTypes()
-                                                                       .stream()
-                                                                       .filter(type -> type.getName()
-                                                                                           .equals(ctx.args(0)
-                                                                                                      .typeBrace(index).type
-                                                                                                   .ID()
-                                                                                                   .getText()))
-                                                                       .findFirst();
+                            .stream()
+                            .filter(type -> type.getName()
+                                    .equals(ctx.args(0)
+                                            .typeBrace(index).type
+                                            .ID()
+                                            .getText()))
+                            .findFirst();
 //                    }
                     if (typee.isEmpty())
                     {
@@ -58,17 +59,18 @@ public class LuluListener extends Lulu2BaseListener
                 {
                     //                            new ArraySignature(ctx.args(0).typeBrace(i).type.ID().getText(), child.size());
                     var typee = ((DeclareScope) currentNode.getScope()).getTypes()
-                                                                       .stream()
-                                                                       .filter(type -> type.getName()
-                                                                                           .equals(ctx.args(0)
-                                                                                                      .typeBrace(index).type
-                                                                                                   .ID()
-                                                                                                   .getText()))
-                                                                       .findFirst();
+                            .stream()
+                            .filter(type -> type.getName()
+                                    .equals(ctx.args(0)
+                                            .typeBrace(index).type
+                                            .ID()
+                                            .getText()))
+                            .findFirst();
 
                 }
             }
         }
+
 //        System.out.println("*******");
 //        System.out.println(ctx.OpenPar());
 //        System.out.println("*******");
@@ -84,6 +86,31 @@ public class LuluListener extends Lulu2BaseListener
     @Override
     public void enterVarDef(VarDefContext ctx)
     {
-        System.out.println(ctx.varVal().size());
+
+        if (ctx.children.get(0).getText().equals(ctx.CONST().getText()))
+        {
+            //TODO :)
+        }
+
     }
+
+    @Override
+    public void enterTypeDcl(TypeDclContext ctx)
+    {
+        var types = ((DeclareScope)currentNode.getScope()).getTypes();
+        boolean duplicate = false;
+        for (var type:types)
+            if((ctx.ID().getText().equals(type.getName())))
+            {
+                duplicate = true;
+                break;
+            }
+        if (duplicate)
+            //the ID has already been added to the list
+            throw new CompileError("Type" + ctx.ID().getText() +" has already been defined in declare scope");
+        //else
+        ((DeclareScope)currentNode.getScope()).addType(new UserDefinedType(ctx.ID().getText()));
+    }
+
+
 }
