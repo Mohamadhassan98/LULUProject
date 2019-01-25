1ad524ae2f706cfdb6ccdb6d313167f606707b74
Mohamadhassan <emohamadhassan@gmail.com>
2019-01-25 16:37:28 +0330

    Phase 1

---
+++ src/LULU/CodeTemp.java
@@ -0,0 +1,19 @@
+package LULU;
+
+import LULU.Lulu2Parser.TypeEnum;
+
+public class CodeTemp
+{
+    public static TypeEnum t = TypeEnum.Float;
+
+    public static void main(String[] args)
+    {
+        int a = 2, b = -0;
+        switch (a)
+        {
+            case 2:
+
+        }
+
+    }
+}
